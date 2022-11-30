eeb94f79e509beb400a944f8d5d88865b4ab8c09
Maximiliano Pollini <lordeddardstark14@gmail.com>
2022-11-29 22:36:52 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let hour = prompt("Enter the time");
+
+if (hour >= 7 && hour < 13) {
+    alert("Good morning!");
+} else if (hour >= 13 && hour < 21) {
+    alert("Good evening!");
+} else {
+    alert("Good night!");
+}
+
+for (let u = 0; u < 3; u++) {
+    console.log("Loop number " + u);
+    alert("Loop number " + u)
+
+    for (let i = 0; i < 2; i++) {
+        console.log("Secondary loop number " + i);
+        alert("Secondary loop number " + i)
+    }
+}

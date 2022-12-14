9cfa240082fdab3be8dccb48229feb4b0a186028
Maximiliano Pollini <lordeddardstark14@gmail.com>
2022-12-13 23:49:16 -0300

    Added basic structure

--- js/main.js
+++ js/main.js
@@ -1,26 +1,22 @@
-let yourname = prompt("Enter your name");
+const products = [
+    { name: "Mouse Logitech G502 Hero", price: 15739 },
+    { name: "Mouse Logitech G600 Gaming Black Rgb", price: 17156 },
+    { name: "Mouse Razer Deathadder v2 Lite", price: 11911 },
+    { name: "Mouse Redragon M988 Storm Elite", price: 10658 },
+    { name: "Keyboard Hyperx Alloy Core Rgb", price: 12471 },
+    { name: "Keyboard Logitech G213 Prodigy Rgb Gaming", price: 18285 },
+    { name: "Keyboard Genesis Thor 400 Rgb", price: 20174 },
+    { name: "Keyboard Redragon K580 Vata Pro Rgb", price: 19999 },
+    { name: "Headphones Redragon H350 Pandora 7.1 Black Rgb", price: 12157 },
+    { name: "Headphones Logitech G335 Gaming Black", price: 19721 },
+    { name: "Headphones Kingston Hyperx Cloud Stinger Core 7.1 Wireless Pc", price: 25108 },
+    { name: "Headphones Genesis Selen 400 Wireless", price: 26301 }
+];
 
-function saludar(yourname) {
-    console.log("Hola " + yourname);
-}
-saludar(yourname);
+console.log(products);
 
-let hour = prompt("Enter the time");
+let desiredproduct = prompt("enter the type of product you want to buy");
+const search = products.filter((product) => product.name.includes(desiredproduct));
+console.log(search);
 
-if (hour >= 7 && hour < 13) {
-    alert("Good morning!");
-} else if (hour >= 13 && hour < 21) {
-    alert("Good evening!");
-} else {
-    alert("Good night!");
-}
-
-for (let u = 0; u < 3; u++) {
-    console.log("Loop number " + u);
-    alert("Loop number " + u)
-
-    for (let i = 0; i < 2; i++) {
-        console.log("Secondary loop number " + i);
-        alert("Secondary loop number " + i)
-    }
-}
+console.log(search[0].price+search[1].price+search[2].price+search[3].price)

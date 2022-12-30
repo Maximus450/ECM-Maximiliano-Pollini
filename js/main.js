360df7fae761518f5590838b48e1164386aeeb90
Maximiliano Pollini <lordeddardstark14@gmail.com>
2022-12-29 23:56:46 -0300

    First cart update

--- js/main.js
+++ js/main.js
@@ -1,22 +1,192 @@
 const products = [
-    { name: "Mouse Logitech G502 Hero", price: 15739 },
-    { name: "Mouse Logitech G600 Gaming Black Rgb", price: 17156 },
-    { name: "Mouse Razer Deathadder v2 Lite", price: 11911 },
-    { name: "Mouse Redragon M988 Storm Elite", price: 10658 },
-    { name: "Keyboard Hyperx Alloy Core Rgb", price: 12471 },
-    { name: "Keyboard Logitech G213 Prodigy Rgb Gaming", price: 18285 },
-    { name: "Keyboard Genesis Thor 400 Rgb", price: 20174 },
-    { name: "Keyboard Redragon K580 Vata Pro Rgb", price: 19999 },
-    { name: "Headphones Redragon H350 Pandora 7.1 Black Rgb", price: 12157 },
-    { name: "Headphones Logitech G335 Gaming Black", price: 19721 },
-    { name: "Headphones Kingston Hyperx Cloud Stinger Core 7.1 Wireless Pc", price: 25108 },
-    { name: "Headphones Genesis Selen 400 Wireless", price: 26301 }
+    { 
+        id: "mouse-01", 
+        name: "Logitech G502 Hero", 
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Mouse",
+            id: "mouse"
+        },
+        price: 15739 
+    },
+    { 
+        id: "mouse-02", 
+        name: "Logitech G600 Gaming Black Rgb", 
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Mouse",
+            id: "mouse"
+        },
+        price: 17156 
+    },
+    { 
+        id: "mouse-03", 
+        name: "Razer Deathadder v2 Lite", 
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Mouse",
+            id: "mouse"
+        },
+        price: 11911 
+    },
+    { 
+        id: "mouse-04", 
+        name: "Redragon M988 Storm Elite", 
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Mouse",
+            id: "mouse"
+        },
+        price: 10658 
+    },
+    { 
+        id: "keyboard-01", 
+        name: "Hyperx Alloy Core Rgb", 
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Keyboard",
+            id: "keyboard"
+        },
+        price: 12471 
+    },
+    { 
+        id: "keyboard-02", 
+        name: "Logitech G213 Prodigy Rgb Gaming", 
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Keyboard",
+            id: "keyboard"
+        },
+        price: 18285 
+    },
+    { 
+        id: "keyboard-03", 
+        name: "Genesis Thor 400 Rgb",  
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Keyboard",
+            id: "keyboard"
+        },
+        price: 20174 
+    },
+    { 
+        id: "keyboard-04", 
+        name: "Redragon K580 Vata Pro Rgb",  
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Keyboard",
+            id: "keyboard"
+        },
+        price: 19999 
+    },
+    { 
+        id: "headphones-01", 
+        name: "Redragon H350 Pandora 7.1 Black Rgb",  
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Headphones",
+            id: "headphones"
+        },
+        price: 12157 
+    },
+    { 
+        id: "headphones-02", 
+        name: "Logitech G335 Gaming Black",  
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Headphones",
+            id: "headphones"
+        },
+        price: 19721 
+    },
+    { 
+        id: "headphones-03", 
+        name: "Kingston Hyperx Cloud Stinger Core 7.1 Wireless Pc",  
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Headphones",
+            id: "headphones"
+        },
+        price: 25108 
+    },
+    { 
+        id: "headphones-04", 
+        name: "Genesis Selen 400 Wireless",  
+        img: "./img/placeholder_250x200.png",
+        category: {
+            name: "Headphones",
+            id: "headphones"
+        },
+        price: 26301 
+    }
 ];
 
-console.log(products);
+const containerProducts = document.querySelector("#products-container");
+let addButton = document.querySelectorAll(".add-product");
+const number = document.querySelector("#number");
 
-let desiredproduct = prompt("enter the type of product you want to buy");
-const search = products.filter((product) => product.name.includes(desiredproduct));
-console.log(search);
+function addProducts(chosenproducts) {
 
-console.log(search[0].price+search[1].price+search[2].price+search[3].price)
+    containerProducts.innerHTML = "";
+
+    chosenproducts.forEach(product => {
+
+        const div = document.createElement("div");
+        div.classList.add("product");
+        div.innerHTML = `
+            <img class="product-img" src="${product.img}" alt="${product.name}">
+            <div class="product-details">
+                <h3 class="product-name">${product.name}</h3>
+                <p class="product-price">$${product.price}</p>
+                <button class="add-product" id="${product.id}">Add to cart</button>
+            </div>
+        `;
+
+        containerProducts.append(div);
+    })
+
+    updateAddButton();
+}
+
+addProducts(products);
+
+function updateAddButton() {
+    addButton = document.querySelectorAll(".add-product");
+
+    addButton.forEach(button => {
+        button.addEventListener("click", addToCart);
+    });
+}
+
+let productsOnCart;
+
+let productsOnCartLS = localStorage.getItem("products-on-cart");
+
+if (productsOnCartLS) {
+    productsOnCart = JSON.parse(productsOnCartLS);
+    updateNumber();
+} else {
+    productsOnCart = [];
+}
+
+function addToCart(e) {
+    const idButton = e.currentTarget.id;
+    const addedProduct = products.find(product => product.id === idButton);
+
+    if(productsOnCart.some(product => product.id === idButton)) {
+        const index = productsOnCart.findIndex(product => product.id === idButton);
+        productsOnCart[index].amount++;
+    } else {
+        addedProduct.amount = 1;
+        productsOnCart.push(addedProduct);
+    }
+
+    updateNumber();
+
+    localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
+}
+
+function updateNumber() {
+    let newNumber = productsOnCart.reduce((acc, product) => acc + product.amount, 0);
+    number.innerText = newNumber;
+}

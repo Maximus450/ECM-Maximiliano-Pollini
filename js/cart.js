9cc2e406609c9b5cbe2014e69c9d8f199f1d7714
Maximiliano Pollini <lordeddardstark14@gmail.com>
2023-01-22 23:15:45 -0300

    CSS libraries and Media

--- js/cart.js
+++ js/cart.js
@@ -12,98 +12,114 @@ const buttonBuy = document.querySelector("#action-cart-buy");
 
 
 function loadProductCart() {
-    if (productsOnCart && productsOnCart.length > 0) {
+  if (productsOnCart && productsOnCart.length > 0) {
 
-        contentEmptyCart.classList.add("disabled");
-        contentProductCart.classList.remove("disabled");
-        contentActionCart.classList.remove("disabled");
-        contentBuyedCart.classList.add("disabled");
+    contentEmptyCart.classList.add("disabled");
+    contentProductCart.classList.remove("disabled");
+    contentActionCart.classList.remove("disabled");
+    contentBuyedCart.classList.add("disabled");
     
-        contentProductCart.innerHTML = "";
+    contentProductCart.innerHTML = "";
     
-        productsOnCart.forEach(product => {
+    productsOnCart.forEach(product => {
     
-            const div = document.createElement("div");
-            div.classList.add("cart-product");
-            div.innerHTML = `
-                <img class="cart-product-img" src="${product.img}" alt="${product.name}">
-                <div class="cart-product-name">
-                    <small>Name</small>
-                    <h3>${product.name}</h3>
-                </div>
-                <div class="cart-product-ammount">
-                    <small>Ammount</small>
-                    <p>${product.ammount}</p>
-                </div>
-                <div class="cart-product-price">
-                    <small>Price</small>
-                    <p>$${product.price}</p>
-                </div>
-                <div class="cart-product-subtotal">
-                    <small>Subtotal</small>
-                    <p>$${product.price * product.ammount}</p>
-                </div>
-                <button class="cart-product-remove" id="${product.id}"><i class="bi bi-trash-fill"></i></button>
-            `;
-    
-            contentProductCart.append(div);
-        })
-    
-    updateButtonRemove();
-    updateTotal();
-	
-    } else {
-        contentEmptyCart.classList.remove("disabled");
-        contentProductCart.classList.add("disabled");
-        contentActionCart.classList.add("disabled");
-        contentBuyedCart.classList.add("disabled");
-    }
+      const div = document.createElement("div");
+      div.classList.add("cart-product");
+      div.innerHTML = `
+        <img class="cart-product-img" src="${product.img}" alt="${product.name}">
+        <div class="cart-product-name">
+          <small>Name</small>
+          <h3>${product.name}</h3>
+        </div>
+        <div class="cart-product-ammount">
+          <small>Ammount</small>
+          <p>${product.ammount}</p>
+        </div>
+        <div class="cart-product-price">
+          <small>Price</small>
+          <p>$${product.price}</p>
+        </div>
+        <div class="cart-product-subtotal">
+          <small>Subtotal</small>
+          <p>$${product.price * product.ammount}</p>
+        </div>
+        <button class="cart-product-remove" id="${product.id}"><i class="bi bi-trash-fill"></i></button>
+      `;
 
+      contentProductCart.append(div);
+    })
+
+  updateButtonRemove();
+  updateTotal();
+
+  } else {
+    contentEmptyCart.classList.remove("disabled");
+    contentProductCart.classList.add("disabled");
+    contentActionCart.classList.add("disabled");
+    contentBuyedCart.classList.add("disabled");
+  }
 }
 
 loadProductCart();
 
 function updateButtonRemove() {
-    buttonRemove = document.querySelectorAll(".cart-product-remove");
+  buttonRemove = document.querySelectorAll(".cart-product-remove");
 
-    buttonRemove.forEach(button => {
-      button.addEventListener("click", removeFromCart);
-    });
+  buttonRemove.forEach(button => {
+    button.addEventListener("click", removeFromCart);
+  });
 }
 
 function removeFromCart(e) {
-    const idButton = e.currentTarget.id;
-    const index = productsOnCart.findIndex(product => product.id === idButton);
-    
-    productsOnCart.splice(index, 1);
-    loadProductCart();
+  const idButton = e.currentTarget.id;
+  const index = productsOnCart.findIndex(product => product.id === idButton);
 
-    localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
+  productsOnCart.splice(index, 1);
+  loadProductCart();
 
+  localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
 }
 
 buttonEmpty.addEventListener("click", emptyCart);
 function emptyCart() {
-  productsOnCart.length = 0;
-    localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
-    loadProductCart();
+
+  Swal.fire({
+    title: 'Do you want empty the cart?',
+    showDenyButton: true,
+    confirmButtonText: 'yes',
+    denyButtonText: `no`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      productsOnCart.length = 0;
+      localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
+      loadProductCart();
+    }
+  })
 }
 
 
 function updateTotal() {
-    const totalCalculated = productsOnCart.reduce((acc, product) => acc + (product.price * product.ammount), 0);
-    total.innerText = `$${totalCalculated}`;
+  const totalCalculated = productsOnCart.reduce((acc, product) => acc + (product.price * product.ammount), 0);
+  total.innerText = `$${totalCalculated}`;
 }
 
 buttonBuy.addEventListener("click", buyCart);
 function buyCart() {
 
-  productsOnCart.length = 0;
-    localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
-    
-    contentEmptyCart.classList.add("disabled");
-    contentProductCart.classList.add("disabled");
-    contentActionCart.classList.add("disabled");
-    contentBuyedCart.classList.remove("disabled");
-
+  Swal.fire({
+    title: 'Do you want to purchase the products?',
+    showDenyButton: true,
+    confirmButtonText: 'yes',
+    denyButtonText: `no`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      productsOnCart.length = 0;
+      localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
+      
+      contentEmptyCart.classList.add("disabled");
+      contentProductCart.classList.add("disabled");
+      contentActionCart.classList.add("disabled");
+      contentBuyedCart.classList.remove("disabled");
+    }
+  })
 }
--- js/main.js
+++ js/main.js
@@ -1,125 +1,11 @@
-const products = [
-    { 
-        id: "mouse-01", 
-        name: "Logitech G502 Hero", 
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Mouse",
-            id: "mouse"
-        },
-        price: 15739 
-    },
-    { 
-        id: "mouse-02", 
-        name: "Logitech G600 Gaming Black Rgb", 
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Mouse",
-            id: "mouse"
-        },
-        price: 17156 
-    },
-    { 
-        id: "mouse-03", 
-        name: "Razer Deathadder v2 Lite", 
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Mouse",
-            id: "mouse"
-        },
-        price: 11911 
-    },
-    { 
-        id: "mouse-04", 
-        name: "Redragon M988 Storm Elite", 
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Mouse",
-            id: "mouse"
-        },
-        price: 10658 
-    },
-    { 
-        id: "keyboard-01", 
-        name: "Hyperx Alloy Core Rgb", 
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Keyboard",
-            id: "keyboard"
-        },
-        price: 12471 
-    },
-    { 
-        id: "keyboard-02", 
-        name: "Logitech G213 Prodigy Rgb Gaming", 
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Keyboard",
-            id: "keyboard"
-        },
-        price: 18285 
-    },
-    { 
-        id: "keyboard-03", 
-        name: "Genesis Thor 400 Rgb",  
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Keyboard",
-            id: "keyboard"
-        },
-        price: 20174 
-    },
-    { 
-        id: "keyboard-04", 
-        name: "Redragon K580 Vata Pro Rgb",  
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Keyboard",
-            id: "keyboard"
-        },
-        price: 19999 
-    },
-    { 
-        id: "headphones-01", 
-        name: "Redragon H350 Pandora 7.1 Black Rgb",  
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Headphones",
-            id: "headphones"
-        },
-        price: 12157 
-    },
-    { 
-        id: "headphones-02", 
-        name: "Logitech G335 Gaming Black",  
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Headphones",
-            id: "headphones"
-        },
-        price: 19721 
-    },
-    { 
-        id: "headphones-03", 
-        name: "Kingston Hyperx Cloud Stinger Core 7.1 Wireless Pc",  
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Headphones",
-            id: "headphones"
-        },
-        price: 25108 
-    },
-    { 
-        id: "headphones-04", 
-        name: "Genesis Selen 400 Wireless",  
-        img: "./img/placeholder_250x200.png",
-        category: {
-            name: "Headphones",
-            id: "headphones"
-        },
-        price: 26301 
-    }
-];
+let products = [];
+
+fetch("./js/products.json")
+  .then(response => response.json())
+  .then(data => {
+    products = data;
+    addProducts(products);
+  })
 
 const containerProducts = document.querySelector("#products-container");
 const buttonCategory = document.querySelectorAll(".button-category");
@@ -129,54 +15,53 @@ const number = document.querySelector("#number");
 
 function addProducts(chosenproducts) {
 
-    containerProducts.innerHTML = "";
+  containerProducts.innerHTML = "";
 
-    chosenproducts.forEach(product => {
+  chosenproducts.forEach(product => {
 
-        const div = document.createElement("div");
-        div.classList.add("product");
-        div.innerHTML = `
-            <img class="product-img" src="${product.img}" alt="${product.name}">
-            <div class="product-details">
-                <h3 class="product-name">${product.name}</h3>
-                <p class="product-price">$${product.price}</p>
-                <button class="add-product" id="${product.id}">Add to cart</button>
-            </div>
-        `;
+    const div = document.createElement("div");
+    div.classList.add("product");
+    div.innerHTML = `
+      <img class="product-img" src="${product.img}" alt="${product.name}">
+      <div class="product-details">
+        <h3 class="product-name">${product.name}</h3>
+        <p class="product-price">$${product.price}</p>
+        <button class="add-product" id="${product.id}">Add to cart</button>
+      </div>
+    `;
 
-        containerProducts.append(div);
-    })
+    containerProducts.append(div);
+  })
 
-    updateAddButton();
+  updateAddButton();
 }
 
-addProducts(products);
 
 buttonCategory.forEach(button => {
-    button.addEventListener("click", (e) => {
+  button.addEventListener("click", (e) => {
 
-        buttonCategory.forEach(button => button.classList.remove("active"));
-        e.currentTarget.classList.add("active");
+    buttonCategory.forEach(button => button.classList.remove("active"));
+    e.currentTarget.classList.add("active");
 
-        if (e.currentTarget.id != "all") {
-            const productCategory = products.find(product => product.category.id === e.currentTarget.id);
-            mainTittle.innerText = productCategory.category.name;
-            const productsButton = products.filter(product => product.category.id === e.currentTarget.id);
-            addProducts(productsButton);
-        } else {
-            mainTittle.innerText = "Products";
-            addProducts(products);
-        }
+    if (e.currentTarget.id != "all") {
+        const productCategory = products.find(product => product.category.id === e.currentTarget.id);
+        mainTittle.innerText = productCategory.category.name;
+        const productsButton = products.filter(product => product.category.id === e.currentTarget.id);
+        addProducts(productsButton);
+    } else {
+        mainTittle.innerText = "Products";
+        addProducts(products);
+    }
 
-    })
+  })
 });
 
 function updateAddButton() {
-    addButton = document.querySelectorAll(".add-product");
+  addButton = document.querySelectorAll(".add-product");
 
-    addButton.forEach(button => {
-        button.addEventListener("click", addToCart);
-    });
+  addButton.forEach(button => {
+    button.addEventListener("click", addToCart);
+  });
 }
 
 let productsOnCart;
@@ -184,30 +69,30 @@ let productsOnCart;
 let productsOnCartLS = localStorage.getItem("products-on-cart");
 
 if (productsOnCartLS) {
-    productsOnCart = JSON.parse(productsOnCartLS);
-    updateNumber();
+  productsOnCart = JSON.parse(productsOnCartLS);
+  updateNumber();
 } else {
-    productsOnCart = [];
+  productsOnCart = [];
 }
 
 function addToCart(e) {
-    const idButton = e.currentTarget.id;
-    const addedProduct = products.find(product => product.id === idButton);
+  const idButton = e.currentTarget.id;
+  const addedProduct = products.find(product => product.id === idButton);
 
-    if(productsOnCart.some(product => product.id === idButton)) {
-        const index = productsOnCart.findIndex(product => product.id === idButton);
-        productsOnCart[index].ammount++;
-    } else {
-        addedProduct.ammount = 1;
-        productsOnCart.push(addedProduct);
-    }
+  if(productsOnCart.some(product => product.id === idButton)) {
+    const index = productsOnCart.findIndex(product => product.id === idButton);
+    productsOnCart[index].ammount++;
+  } else {
+    addedProduct.ammount = 1;
+    productsOnCart.push(addedProduct);
+  }
 
-    updateNumber();
+  updateNumber();
 
-    localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
+  localStorage.setItem("products-on-cart", JSON.stringify(productsOnCart));
 }
 
 function updateNumber() {
-    let newNumber = productsOnCart.reduce((acc, product) => acc + product.ammount, 0);
-    number.innerText = newNumber;
+  let newNumber = productsOnCart.reduce((acc, product) => acc + product.ammount, 0);
+  number.innerText = newNumber;
 }
